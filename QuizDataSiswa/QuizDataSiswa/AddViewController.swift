9014ebdc72053458489bbbf3d7d743688c4c7f66
nadenaa <32922893+nadenaa@users.noreply.github.com>
2017-10-26 13:15:22 +0700

    QuizDAtASiswa

---
+++ QuizDataSiswa/QuizDataSiswa/AddViewController.swift
@@ -0,0 +1,112 @@
+//
+//  AddViewController.swift
+//  QuizDataSiswa
+//
+//  Created by yusronadena on 10/26/17.
+//  Copyright © 2017 yusron. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    
+    @IBOutlet weak var etYourAge: UITextField!
+    @IBOutlet weak var etYourlass: UITextField!
+    @IBOutlet weak var etYoourSchool: UITextField!
+    @IBOutlet weak var etPassword: UITextField!
+    @IBOutlet weak var etEmail: UITextField!
+    @IBOutlet weak var etUsername: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func btnSubmit(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let nameTask = Task(context: context) //deklarasi nameTask sebagai konteks dari entiti Task
+        nameTask.username = etUsername.text //deklarasikan bahwa nameTask.name_task itu isinya dari etNametask.text
+        
+        nameTask.email = etEmail.text
+        nameTask.password = etPassword.text
+        nameTask.yourschool = etYoourSchool.text
+        nameTask.yourclass = etYourlass.text
+        nameTask.yourage = etYourAge.text
+        
+        if etUsername.text == "" {
+            let alert = UIAlertController(title: "Warning", message: "Task Cannot Be Empty", preferredStyle: UIAlertControllerStyle.alert)
+            //menambahkan aksi ke button
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else if etEmail.text == "" {
+            //kondisi ketika kosong
+            //tampil alert dialog
+            let alert = UIAlertController(title: "Warning", message: "Task Cannot Be Empty", preferredStyle: UIAlertControllerStyle.alert)
+            //menambahkan aksi ke button
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else if etPassword.text == "" {
+            //kondisi ketika kosong
+            //tampil alert dialog
+            let alert = UIAlertController(title: "Warning", message: "Task Cannot Be Empty", preferredStyle: UIAlertControllerStyle.alert)
+            //menambahkan aksi ke button
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else if etPassword.text == "" {
+            //kondisi ketika kosong
+            //tampil alert dialog
+            let alert = UIAlertController(title: "Warning", message: "Task Cannot Be Empty", preferredStyle: UIAlertControllerStyle.alert)
+            //menambahkan aksi ke button
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else if etYoourSchool.text == "" {
+            //kondisi ketika kosong
+            //tampil alert dialog
+            let alert = UIAlertController(title: "Warning", message: "Task Cannot Be Empty", preferredStyle: UIAlertControllerStyle.alert)
+            //menambahkan aksi ke button
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else if etYourlass.text == "" {
+            //kondisi ketika kosong
+            //tampil alert dialog
+            let alert = UIAlertController(title: "Warning", message: "Task Cannot Be Empty", preferredStyle: UIAlertControllerStyle.alert)
+            //menambahkan aksi ke button
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else if etYourAge.text == "" {
+            //kondisi ketika kosong
+            //tampil alert dialog
+            let alert = UIAlertController(title: "Warning", message: "Task Cannot Be Empty", preferredStyle: UIAlertControllerStyle.alert)
+            //menambahkan aksi ke button
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }else{
+            //ketika kondisi tesk task nya tidak kosong
+            //data di simpan ke core Data
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            _ = navigationController?.popViewController(animated: true)
+            
+            //mencetak kalau data berhasil ditambahkan
+            print("Data berhasil disimpan")
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ QuizDataSiswa/QuizDataSiswa/NoteTableViewController.swift
@@ -0,0 +1,157 @@
+//
+//  NoteTableViewController.swift
+//  QuizDataSiswa
+//
+//  Created by yusronadena on 10/26/17.
+//  Copyright © 2017 yusron. All rights reserved.
+//
+
+import UIKit
+
+class NoteTableViewController: UITableViewController {
+    
+    var tasks : [Task] = [] //Task ini di panggil dari entiti yang sudah di buat sebelumnya
+    
+    //dekarasikan context object untuk persistnet container
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tasks.count
+    }
+
+   
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellTable", for: indexPath) as!
+        serahTableViewCell
+
+        // Configure the cell...
+        let dataTask = tasks[indexPath.row]
+        //mengambildata dengan attribute name_Task
+        if let myDataTask = dataTask.username {
+            //menampilkan data ke label
+            cell.labelUsername.text = myDataTask
+        }
+        if let myDataTask1 = dataTask.email {
+            cell.labelEmail.text = myDataTask1
+        }
+     
+        if let myDataTask3 = dataTask.yourschool {
+            cell.labelSchool.text = myDataTask3
+        }
+    
+        
+
+        return cell
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        //memanggil method getData
+        getData()
+        //memanggil reloadData
+        tableView.reloadData()
+    }
+    
+    //method getData
+    func getData() {
+        //mengecek apakah ada error atau tidak
+        do{
+            //kondisi kalau tidak ada error
+            //maka akan request download data
+            tasks = try context.fetch(Task.fetchRequest())
+        }
+        catch{
+            //kondisi apabila error fetch data
+            print("Fetching Failed")
+        }
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath){
+        //mengece menu swipe bila edit data
+        if editingStyle == .delete {
+            let task = tasks[indexPath.row]
+            context.delete(task)
+            //Delete data
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            do{
+                //retrieve data
+                tasks = try context.fetch(Task.fetchRequest())
+            }
+            catch{
+                print("Fetching Failed")
+            }
+        }
+        //load data lagi
+        tableView.reloadData()
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+}
